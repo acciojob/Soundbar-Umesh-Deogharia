b2b2eeb96a3af196f98d12fba6d81cf716be28fc
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-10-25 13:40:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,26 @@
-//your JS code here. If required.
-let buttons = document.querySelectorAll(".btn");
-let stop = document.querySelector(".stop");
-let obj = {
-  tag: "",
-  check: false,
-};
+const buttons= document.querySelectorAll(".btn");
+const stop= document.querySelector(".stop");
+ 
+let currentAudio= null;
+ 
+buttons.forEach(button => {
+	button.addEventListener("click", () => {
+		const sound = button.getAttribute("data-sound");
+ 
+		if(currentAudio) {
+			currentAudio.pause();
+			currentAudio.currentTime = 0;
+		}
+ 
+		currentAudio = new Audio(sound);
+		document.body.appendChild(currentAudio);  
+		currentAudio.play();
+	})
+})
+ 
 stop.addEventListener("click", () => {
-  stopSong();
-});
-
-buttons.forEach((elem) => {
-  elem.addEventListener("click", (e) => {
-    if (obj.check === true) {
-      stopSong();
-    }
-
-    addAudio(e, elem);
-  });
-});
-
-function addAudio(e, elem) {
-	let existingAudio = elem.querySelector('audio');
-  if (existingAudio) {
-    elem.removeChild(existingAudio);
-  }
-  obj.check = true;
-  obj.tag = elem;
-  let target = e.target.dataset.sound;
-  let audio = document.createElement("audio");
-  let source = document.createElement("source");
-  source.src = `./${target}`;
-  audio.setAttribute("autoplay", true);
-
-  audio.appendChild(source);
-  elem.appendChild(audio);
-}
-
-function stopSong() {
-  let Text = obj.tag.innerText;
-
-  obj.tag.innerHTML = "";
-  obj.tag.innerText = Text;
-  //   toggle = false;
-  obj.check = false;
-  obj.tag = "";
-}
-// console.log(buttons);
+	if(currentAudio) {
+		currentAudio.pause();
+		currentAudio.currentTime = 0;
+	}
+})
